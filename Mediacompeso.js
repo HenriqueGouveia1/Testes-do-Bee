084848d73a10ca0d5b9b25b766008d739597a355
henriquegouveia1 <henriquegs002@gmail.com>
2022-03-24 16:56:51 -0300

    beecrownd 1005

---
+++ Mediacompeso.js
@@ -0,0 +1,14 @@
+var input = require('fs').readFileSync('./stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+var A = parseFloat(lines.shift()).toFixed(1)
+var B = parseFloat(lines.shift()).toFixed(1)
+
+var media = (A*3.5 + B*7.5)/11
+
+console.log("MEDIA = "+media.toFixed(5))
