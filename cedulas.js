2198e2c10908075fd0fae08df31139eac93706d5
henriquegouveia1 <henriquegs002@gmail.com>
2022-03-30 21:51:07 -0300

    cedulas

--- cedulas.js
+++ cedulas.js
@@ -1,3 +1,13 @@
 //1018
 var input = require('fs').readFileSync('./stdin', 'utf8');
-var lines = input.split('\n');
+//var lines = input.split('\n');
+
+const arr = [100, 50, 20, 10, 5, 2 ,1]
+let valor = parseInt(input)
+
+for(let nota of arr){
+    let qtd = parseInt(valor / nota)
+    console.log(`${qtd} nota(s) de R$ ${nota}`)
+    valor = valor % nota
+
+}
